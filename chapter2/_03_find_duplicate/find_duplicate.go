880d1242f6269faa932bfbe4da6eba36edd520ea
hq <hq_cml@163.com>
2021-04-25 20:44:26 +0800

    find duplicate

--- chapter2/_03_find_duplicate/find_duplicate.go
+++ chapter2/_03_find_duplicate/find_duplicate.go
@@ -1,5 +1,7 @@
 package _03_find_duplicate
 
+import "errors"
+
 //空间换时间，利用数组作为hash
 //返回，exist标识是否重复，d标识重复的数，如果存在
 func FindDuplicate0(s []int) (d int, exist bool){
@@ -48,4 +50,51 @@ func FindDuplicate1(s []int) (d int, exist bool){
 	}
 
 	return -1, false
+}
+
+//2 3 5 4 3 2 6 7
+//6 1 2 3 4 5 6 7
+func countRange(s []int, min, max int) int {
+	i := 0
+	for _, v := range s {
+		if v >= min && v <= max {
+			i++
+		}
+	}
+	return i
+}
+
+func FindDuplicate2 (s []int) (int, bool, error) {
+	n := len(s)
+	if n <= 1 { //数组为空或者仅有一个元素，则必然不重复
+		return -1, false, nil
+	}
+
+	//检验数据合法性
+	for i:=0; i<n; i++ {
+		if s[i] < 1 || s[i] > (n-1) {
+			return -1, false, errors.New("Ivalid number")
+		}
+	}
+
+	beg := 1
+	end := n-1
+
+	for beg <= end { //n >= 2，所以end最小是1
+		if beg == end {
+			return beg, true, nil
+		}
+
+		mid := (end-beg)>>1 + beg
+		num := countRange(s, beg, mid)
+		if num <= (mid - beg + 1) {
+			//重复数字，不可能在此，则换另一半进行探测
+			beg = mid + 1
+		} else {
+			//重复有可能在此，则继续探测
+			end = mid
+		}
+
+	}
+	return -1, false, nil
 }
--- chapter2/_03_find_duplicate/find_duplicate_test.go
+++ chapter2/_03_find_duplicate/find_duplicate_test.go
@@ -57,3 +57,59 @@ func TestFindDuplicate(t *testing.T) {
 		})
 	}
 }
+
+func TestFindDuplicate2(t *testing.T) {
+	type args struct {
+		s []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		want1   bool
+		wantErr bool
+	}{
+		{
+			name:    "case1",
+			args:    args{
+				[]int{2, 3, 5, 4, 3, 2, 6, 7},
+			},
+			want:    3,
+			want1:   true,
+			wantErr: false,
+		},
+		{
+			name:    "case2",
+			args:    args{
+				[]int{6, 1, 2, 3, 4, 5, 6, 7},
+			},
+			want:    6,
+			want1:   true,
+			wantErr: false,
+		},
+		{
+			name:    "case3",
+			args:    args{
+				[]int{1, 2},
+			},
+			want:    -1,
+			want1:   false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := FindDuplicate2(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindDuplicate2() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FindDuplicate2() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("FindDuplicate2() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
