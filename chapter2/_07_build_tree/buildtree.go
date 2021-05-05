4add0c2ad717ea0d2a37323358770adac6a548d2
hq <hq_cml@163.com>
2021-05-05 14:57:04 +0800

    build tree

---
+++ chapter2/_07_build_tree/buildtree.go
@@ -0,0 +1,38 @@
+package _07_build_tree
+
+import (
+	"errors"
+	"github.com/hq-cml/sward2offer/common"
+)
+
+//pre: 1, 2, 4, 7, 3, 5, 6, 8
+//mid: 4, 7, 2, 1, 5, 3, 8, 6
+func BuildTree(pre, mid []int) (*common.Node, error) {
+	if len(pre) != len(mid) {
+		return nil, errors.New("valid pre & mid")
+	}
+
+	length := len(pre)
+	if length == 0 {
+		return nil, nil
+	}
+
+	head := pre[0]
+	idx := 0
+	for i, v := range mid {
+		if v == head {
+			idx = i
+			break
+		}
+	}
+
+	node := common.NewNode(head)
+	var err1 error
+	var err2 error
+	node.Left, err1 = BuildTree(pre[1 : idx+1], mid[0 : idx])
+	node.Right, err2 = BuildTree(pre[idx+1:], mid[idx+1:])
+	if err1 != nil || err2 != nil {
+		return nil, errors.New(err1.Error() + err2.Error())
+	}
+	return node, nil
+}
---
+++ chapter2/_07_build_tree/buildtree_test.go
@@ -0,0 +1,45 @@
+package _07_build_tree
+
+import (
+	"fmt"
+	"swordoff/common"
+	"testing"
+)
+
+func TestBuildTree(t *testing.T) {
+	type args struct {
+		pre []int
+		mid []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *common.Node
+		wantErr bool
+	}{
+		{
+			name:    "case1",
+			args:    args{
+				pre: []int{1, 2, 4, 7, 3, 5, 6, 8},
+				mid: []int{4, 7, 2, 1, 5, 3, 8, 6},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := BuildTree(tt.args.pre, tt.args.mid)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildTree() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			common.Pre(got); fmt.Println()
+			common.Mid(got); fmt.Println()
+			common.Post(got); fmt.Println()
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("BuildTree() got = %v, want %v", got, tt.want)
+			//}
+		})
+	}
+}
