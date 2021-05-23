5ca16db3a61e5f4d775c297474a1b61d76e94132
hq <hq_cml@163.com>
2021-05-23 10:52:28 +0800

    list

--- common/list.go
+++ common/list.go
@@ -1,6 +1,55 @@
 package common
 
+import "fmt"
+
 type ListNode struct {
     Val int
     Next *ListNode
+}
+
+//入队列，队尾
+func (l *ListNode)PushNode(v int) *ListNode {
+    node := &ListNode{
+        Val:  v,
+        Next: nil,
+    }
+    if l == nil {
+        return node
+    }
+    p := l
+    for p.Next != nil {
+        p = p.Next
+    }
+    p.Next = node
+
+    return l
+}
+
+//出队列，队头
+func (l *ListNode)PopNode() (int, *ListNode, bool) {
+    if l == nil {
+        return 0, nil, false
+    }
+
+    return l.Val, l.Next, true
+}
+
+//遍历
+func (l *ListNode)Foreach(f func(*ListNode)) {
+    p := l
+    for p != nil {
+        f(p)
+        p = p.Next
+    }
+}
+
+func nodePrint(node *ListNode) {
+    if node == nil {
+        return
+    }
+    if node.Next == nil {
+        fmt.Println(node.Val)
+    } else {
+        fmt.Print(node.Val, " ")
+    }
 }
---
+++ common/list_test.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestListNode_Foreach(t *testing.T) {
+    type args struct {
+        f func(*ListNode)
+    }
+    tests := []struct {
+        name   string
+        args   args
+    }{
+        {
+            name:   "case1",
+            args:   args{
+                f: nodePrint,
+            },
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            var l *ListNode
+            l = l.PushNode(2)
+            l = l.PushNode(4)
+            l = l.PushNode(5)
+            l = l.PushNode(8)
+            l.Foreach(tt.args.f)
+            v, l, b := l.PopNode()
+            if b != true {
+                fmt.Println("err:", b)
+            }
+            fmt.Println("Pop:", v)
+            l.Foreach(tt.args.f)
+        })
+    }
+}
