c7946fde808d9ad094f3ec872bd0502a942e43b8
hq <hq_cml@163.com>
2021-05-25 08:17:35 +0800

    tree mirror

---
+++ chapter4/_27_tree_mirror/mirror.go
@@ -0,0 +1,12 @@
+package _27_tree_mirror
+
+import "github.com/hq-cml/sward2offer/common"
+
+func Mirror(root *common.Node) {
+	if root == nil {
+		return
+	}
+	root.Left, root.Right = root.Right, root.Left
+	Mirror(root.Left)
+	Mirror(root.Right)
+}
---
+++ chapter4/_27_tree_mirror/mirror_test.go
@@ -0,0 +1,45 @@
+package _27_tree_mirror
+
+import (
+	"fmt"
+	"github.com/hq-cml/sward2offer/common"
+	"testing"
+)
+
+func TestMirror(t *testing.T) {
+	type args struct {
+		root *common.Node
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "case1",
+			args: args{
+				root: common.NewNodeWithChild(1,
+					common.NewNodeWithChild(2,
+						common.NewNode(4),
+						common.NewNode(5)),
+					common.NewNodeWithChild(3,
+						nil,
+						common.NewNode(6))),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			common.Pre(tt.args.root)
+			fmt.Println()
+			common.Mid(tt.args.root)
+			fmt.Println()
+
+			Mirror(tt.args.root)
+
+			common.Pre(tt.args.root)
+			fmt.Println()
+			common.Mid(tt.args.root)
+			fmt.Println()
+		})
+	}
+}
