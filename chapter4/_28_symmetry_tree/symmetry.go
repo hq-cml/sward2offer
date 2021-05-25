c3cf05274373ab297d3f3e7018583f2745b9c4e9
hq <hq_cml@163.com>
2021-05-25 08:18:23 +0800

    tree symmetry

---
+++ chapter4/_28_symmetry_tree/symmetry.go
@@ -0,0 +1,25 @@
+package _28_symmetry_tree
+
+import (
+	"github.com/hq-cml/sward2offer/common"
+)
+
+func Symmetry(root *common.Node) bool {
+	return symmetry(root, root)
+}
+
+func symmetry(root1, root2 *common.Node) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+
+	if root1.I != root2.I {
+		return false
+	}
+
+	return symmetry(root1.Left, root2.Right) &&
+		symmetry(root1.Right, root2.Left)
+}
---
+++ chapter4/_28_symmetry_tree/symmetry_test.go
@@ -0,0 +1,51 @@
+package _28_symmetry_tree
+
+import (
+	"github.com/hq-cml/sward2offer/common"
+	"testing"
+)
+
+func TestSymmetry(t *testing.T) {
+	type args struct {
+		root *common.Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "case1",
+			args: args{
+				root: common.NewNodeWithChild(1,
+					common.NewNodeWithChild(2,
+						common.NewNode(4),
+						common.NewNode(5)),
+					common.NewNodeWithChild(3,
+						nil,
+						common.NewNode(6))),
+			},
+			want: false,
+		},
+		{
+			name: "case2",
+			args: args{
+				root: common.NewNodeWithChild(1,
+					common.NewNodeWithChild(2,
+						common.NewNode(4),
+						common.NewNode(5)),
+					common.NewNodeWithChild(2,
+						common.NewNode(5),
+						common.NewNode(4))),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Symmetry(tt.args.root); got != tt.want {
+				t.Errorf("Symmetry() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
