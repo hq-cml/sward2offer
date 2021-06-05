6d8b5daeee42c9692b5e2333eee0138f05fc014e
hq <hq_cml@163.com>
2021-06-05 23:59:18 +0800

    find min k

---
+++ chapter5/_40_find_min_k/find.go
@@ -0,0 +1,68 @@
+package _40_find_min_k
+
+import (
+	"errors"
+	"github.com/hq-cml/sward2offer/basic"
+	"github.com/hq-cml/sward2offer/common"
+)
+
+func FindMinK1(arr []int, k int) ([]int, error) {
+	if len(arr) == 0 {
+		return nil, errors.New("invalid")
+	}
+
+	if k <=0 || k > len(arr) {
+		return nil, errors.New("invalid")
+	}
+
+	if k == len(arr) {
+		return arr, nil
+	}
+
+	beg := 0
+	end := len(arr) - 1
+	for beg <= end {
+		idx := basic.Partition(arr, beg, end)
+		if idx == k-1 {
+			return arr[:k], nil
+		}
+
+		if idx > k-1 {
+			end = idx - 1
+		} else {
+			beg = idx + 1
+		}
+	}
+
+	return nil, errors.New("Something wrong")
+}
+
+//利用大顶堆
+func FindMinK2(arr []int, k int) ([]int, error) {
+	if len(arr) == 0 {
+		return nil, errors.New("invalid")
+	}
+
+	if k <=0 || k > len(arr) {
+		return nil, errors.New("invalid")
+	}
+
+	if k == len(arr) {
+		return arr, nil
+	}
+
+	heap := common.NewMaxHeapInt(nil)
+	for _, v := range arr {
+		if heap.Len() < k {
+			heap.Put(v)
+		} else {
+			top := heap.Get(0)
+			if top > v {
+				heap.Remove(0)
+				heap.Put(v)
+			}
+		}
+	}
+
+	return heap.Arr(), nil
+}
---
+++ chapter5/_40_find_min_k/find_test.go
@@ -0,0 +1,142 @@
+package _40_find_min_k
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFindMinK1(t *testing.T) {
+	type args struct {
+		arr []int
+		k   int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []int
+		wantErr bool
+	}{
+		{
+			name:    "case1",
+			args:    args{
+				arr: []int{4,1,3,2,6,5,7,8},
+				k:   4,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case2",
+			args:    args{
+				arr: []int{4,1,3,2,6,5,7,8},
+				k:   5,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case3",
+			args:    args{
+				arr: []int{4,1,3,2,6,5,7,8},
+				k:   1,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case4",
+			args:    args{
+				arr: []int{4,1,4,4,4,3,2,6,5,7,8},
+				k:   4,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case5",
+			args:    args{
+				arr: []int{4,1,4,4,4,3,2,6,5,7,8},
+				k:   5,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FindMinK1(tt.args.arr, tt.args.k)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindMinK1() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println(got)
+		})
+	}
+}
+
+func TestFindMinK2(t *testing.T) {
+	type args struct {
+		arr []int
+		k   int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []int
+		wantErr bool
+	}{
+		{
+			name:    "case1",
+			args:    args{
+				arr: []int{4,1,3,2,6,5,7,8},
+				k:   4,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case2",
+			args:    args{
+				arr: []int{4,1,3,2,6,5,7,8},
+				k:   5,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case3",
+			args:    args{
+				arr: []int{4,1,3,2,6,5,7,8},
+				k:   1,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case4",
+			args:    args{
+				arr: []int{4,1,4,4,4,3,2,6,5,7,8},
+				k:   4,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case5",
+			args:    args{
+				arr: []int{4,1,4,4,4,3,2,6,5,7,8},
+				k:   5,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "case5",
+			args:    args{
+				arr: []int{4,1,4,4,4,3,2,6,5,7,8},
+				k:   6,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FindMinK2(tt.args.arr, tt.args.k)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindMinK1() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println(got)
+		})
+	}
+}
