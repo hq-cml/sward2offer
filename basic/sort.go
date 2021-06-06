0391dce475718291dd417865b58e04e0550411c8
hq <hq_cml@163.com>
2021-06-06 10:08:49 +0800

    quick sort & partition

--- basic/sort.go
+++ basic/sort.go
@@ -1,7 +1,6 @@
 package basic
 
-import "fmt"
-
+//快速排序
 func QuickSort(arr []int) {
     if len(arr) == 0{
         return
@@ -11,40 +10,8 @@ func QuickSort(arr []int) {
     if mid == -1 {
         return
     }
-    fmt.Println("X----",mid)
+
+    //递归
     QuickSort(arr[0:mid])
-    //if mid < len(arr) -1 {
-        QuickSort(arr[mid+1:])
-   // }
-
-}
-
-//一次分离
-func Partition(arr []int, begIdx, endIdx int) (int){
-    if len(arr) == 0 {
-        return -1
-    }
-    //if begIdx > endIdx || begIdx < 0 || endIdx >= len(arr) {
-    //    return -1
-    //}
-
-    val := arr[begIdx]
-
-    for endIdx > begIdx {
-        for arr[endIdx] >= val && endIdx > begIdx {
-            endIdx --
-            continue
-        }
-        //swap
-        arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
-
-        for arr[begIdx] <= val && endIdx > begIdx {
-            begIdx ++
-            continue
-        }
-        //swap
-        arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
-    }
-
-    return begIdx
+    QuickSort(arr[mid+1:])
 }
