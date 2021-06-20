7937ec27a4ddbd902b390455449204e4660e46a2
hq <hq_cml@163.com>
2021-06-20 09:07:07 +0800

    joseph_ring

---
+++ chapter6/_62_joseph_ring/list_ring.go
@@ -0,0 +1,48 @@
+package _62_joseph_ring
+
+import (
+    "errors"
+    "fmt"
+    "github.com/hq-cml/sward2offer/common"
+)
+
+//约瑟夫环
+//难度：2*
+func CalclRing(n, m int) (int, error) {
+    if n <= 0 || m <= 0 {
+        return -1, errors.New("Invalid err")
+    }
+    l := common.NewList()
+    l = l.PushNode(0)
+    head := l
+    var node *common.ListNode
+    for i:=1; i<n; i++ { //构造环
+        node = &common.ListNode{
+            Val:  i,
+            Next: nil,
+        }
+        l = l.PushNodePtr(node)
+    }
+    if node != nil {
+        node.Next = head
+    } else {
+        head.Next = head  //单节点成环
+    }
+
+    var pre *common.ListNode
+    p := head
+    for p.Next != p { //剩下不止一个节点
+        for i:=0; i<m-1; i++ {
+            pre = p
+            p = p.Next
+        }
+        pre.Next = p.Next
+        fmt.Println("Del:", p.Val)
+        p = p.Next
+    }
+    return p.Val, nil
+}
+
+//方法2：
+//作者分析出一个公式。。。
+//这就只能望洋兴叹了。。。
---
+++ chapter6/_62_joseph_ring/list_ring_test.go
@@ -0,0 +1,38 @@
+package _62_joseph_ring
+
+import "testing"
+
+func TestCalclRing(t *testing.T) {
+    type args struct {
+        n int
+        m int
+    }
+    tests := []struct {
+        name    string
+        args    args
+        want    int
+        wantErr bool
+    }{
+        {
+            name:    "case1",
+            args:    args{
+                n: 5,
+                m: 3,
+            },
+            want:    3,
+            wantErr: false,
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got, err := CalclRing(tt.args.n, tt.args.m)
+            if (err != nil) != tt.wantErr {
+                t.Errorf("CalclRing() error = %v, wantErr %v", err, tt.wantErr)
+                return
+            }
+            if got != tt.want {
+                t.Errorf("CalclRing() got = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
