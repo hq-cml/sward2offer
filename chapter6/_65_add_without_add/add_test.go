35deede60cac51ba414d637ab7bc14cfbdcdd47b
hq <hq_cml@163.com>
2021-06-23 08:30:58 +0800

    add_without_add

---
+++ chapter6/_65_add_without_add/add_test.go
@@ -0,0 +1,38 @@
+package _65_add_without_add
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+    type args struct {
+        i int
+        j int
+    }
+    tests := []struct {
+        name string
+        args args
+        want int
+    }{
+        {
+            name: "case1",
+            args: args{
+                i: 101,
+                j: 202,
+            },
+            want: 303,
+        },
+        {
+            name: "case2",
+            args: args{
+                i: -5,
+                j: 11,
+            },want: 6,
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := Add(tt.args.i, tt.args.j); got != tt.want {
+                t.Errorf("Add() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
