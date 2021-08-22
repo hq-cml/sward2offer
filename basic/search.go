12eed9951481cc158f5ebbb4f91ebeba82fb467f
hq <hq_cml@163.com>
2021-08-22 17:51:12 +0800

    binary search

---
+++ basic/search.go
@@ -0,0 +1,23 @@
+package basic
+
+func BinarySearch(arr []int, need int) int {
+    if len(arr) == 0 {
+        return -1
+    }
+
+    beg := 0
+    end := len(arr)-1
+
+    for beg <= end {
+        mid := (end-beg)/2 + beg
+        if arr[mid] == need {
+            return mid
+        } else if arr[mid] > need {
+            end = mid - 1
+        } else {
+            beg = mid + 1
+        }
+    }
+
+    return -1
+}
