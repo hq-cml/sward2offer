d38311174bfc36d0b114b84832fe35d86e685f16
hq <hq_cml@163.com>
2021-09-05 18:33:26 +0800

    basic comment

--- basic/common.go
+++ basic/common.go
@@ -1,31 +1,30 @@
 package basic
 
 //分区函数
-func Partition(arr []int, begIdx, endIdx int) (int){
-    if len(arr) == 0 {
-        return -1
-    }
-    if begIdx > endIdx || begIdx < 0 || endIdx >= len(arr) {
-        return -1
-    }
+//返回：一次分区完毕之后的中间Idx值，保证Idx前面的均小于Idx元素值，后面的均大于
+func Partition(arr []int, begIdx, endIdx int) int {
+	if len(arr) == 0 {
+		return -1
+	}
+	if begIdx > endIdx || begIdx < 0 || endIdx >= len(arr) {
+		return -1
+	}
 
-    val := arr[begIdx]
+	val := arr[begIdx]
 
-    for endIdx > begIdx {
-        for arr[endIdx] >= val && endIdx > begIdx {
-            endIdx --
-            continue
-        }
-        //swap
-        arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
+	for endIdx > begIdx {
+		for arr[endIdx] >= val && endIdx > begIdx {
+			endIdx--
+		}
+		//swap
+		arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
 
-        for arr[begIdx] <= val && endIdx > begIdx {
-            begIdx ++
-            continue
-        }
-        //swap
-        arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
-    }
+		for arr[begIdx] <= val && endIdx > begIdx {
+			begIdx++
+		}
+		//swap
+		arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
+	}
 
-    return begIdx
+	return begIdx
 }
--- basic/sort.go
+++ basic/sort.go
@@ -4,50 +4,50 @@ import "github.com/hq-cml/sward2offer/common"
 
 //冒泡排序
 func BubbleSort(arr []int) {
-    if len(arr) == 0{
-        return
-    }
+	if len(arr) == 0 {
+		return
+	}
 
-    for i:=1; i<len(arr); i++ {
-        for j:=0; j<len(arr)-i; j ++ {
-            if arr[j] > arr[j+1] {
-                arr[j], arr[j+1] = arr[j+1], arr[j] //swap
-            }
-        }
-    }
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j] //swap
+			}
+		}
+	}
 }
 
 //快速排序
 func QuickSort(arr []int) {
-    if len(arr) == 0{
-        return
-    }
+	if len(arr) == 0 {
+		return
+	}
 
-    mid := Partition(arr, 0, len(arr)-1)
-    if mid == -1 {
-        return
-    }
+	mid := Partition(arr, 0, len(arr)-1)
+	if mid == -1 {
+		return
+	}
 
-    //递归
-    QuickSort(arr[0:mid])  //不包括mid
-    QuickSort(arr[mid+1:]) //不包括mid
+	//递归
+	QuickSort(arr[0:mid])  //不包括mid
+	QuickSort(arr[mid+1:]) //不包括mid
 }
 
 //堆排序
 func HeapSort(arr []int) []int {
-    if len(arr) == 0{
-        return arr
-    }
+	if len(arr) == 0 {
+		return arr
+	}
 
-    s := []int{}
-    heap := common.NewHeapInt(s, true)
-    for _, v := range arr {
-        heap.Push(v)
-    }
+	s := []int{}
+	heap := common.NewHeapInt(s, true) //小顶堆
+	for _, v := range arr {
+		heap.Push(v)
+	}
 
-    ret := []int{}
-    for heap.Len() > 0 {
-        ret = append(ret, heap.Remove(0))
-    }
-    return ret
+	ret := []int{}
+	for heap.Len() > 0 {
+		ret = append(ret, heap.Remove(0)) //逐个从小顶堆移出
+	}
+	return ret
 }
