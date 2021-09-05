592743b4a4833f199b0444d18b3271f3ba6ad074
hq <hq_cml@163.com>
2021-09-05 22:57:41 +0800

    chapter7 comment

--- chapter7/atoi.go
+++ chapter7/atoi.go
@@ -6,9 +6,9 @@
 package chapter7
 
 import (
-    "errors"
-    "math"
-    "strings"
+	"errors"
+	"math"
+	"strings"
 )
 
 //思路：主要是各种边界和异常考虑的全面性
@@ -17,102 +17,102 @@ import (
 //3. 越界问题
 //4. 不支持浮点小数
 func Atoi(str string) (int, error) {
-    //过滤空格字符
-    str = strings.Trim(str, " ")
-    if str == "" {
-        return 0, errors.New("Invalid input")
-    }
+	//过滤空格字符
+	str = strings.Trim(str, " ")
+	if str == "" {
+		return 0, errors.New("Invalid input")
+	}
 
-    //正负号
-    sign := 1
-    if str[0] == '+' {
-        str = str[1:]
-    } else if string(str[0]) == "-" {
-        str = str[1:]
-        sign = -1
-    }
+	//正负号
+	sign := 1
+	if str[0] == '+' {
+		str = str[1:]
+	} else if string(str[0]) == "-" {
+		str = str[1:]
+		sign = -1
+	}
 
-    //仅有正负号
-    if len(str) == 0 {
-        return 0, errors.New("Invalid input")
-    }
+	//仅有正负号
+	if len(str) == 0 {
+		return 0, errors.New("Invalid input")
+	}
 
-    ret := 0
-    for i:=0; i<len(str); i++ {
-        if str[i] >= '0' && str[i] <= '9' {
-            ret = ret * 10 + int(str[i] - '0')
-            if checkOverflow(ret, sign==-1) {
-                return 0, errors.New("Over flow")
-            }
-        } else {
-            return 0, errors.New("Invalid input")
-        }
-    }
-    return sign * ret, nil
+	ret := 0
+	for i := 0; i < len(str); i++ {
+		if str[i] >= '0' && str[i] <= '9' {
+			ret = ret*10 + int(str[i]-'0')
+			if checkOverflow(ret, sign == -1) {
+				return 0, errors.New("Over flow")
+			}
+		} else {
+			return 0, errors.New("Invalid input")
+		}
+	}
+	return sign * ret, nil
 }
 
 //校验是否溢出
 //最大的有符号数（32位）：0x7FFFFFFF == math.MaxInt32
 //最小的有符号数（32位）：0x80000000 == math.MinInt32
 func checkOverflow(n int, minus bool) bool {
-    if !minus {
-        return n > math.MaxInt64
-    } else {
-        return n < math.MinInt64
-    }
+	if !minus {
+		return n > math.MaxInt64
+	} else {
+		return n < math.MinInt64
+	}
 }
 
 //带四舍五入的Atoi
 //支持对于小数的四舍五入
 func AtoiApproximate(str string) (int, error) {
-    //过滤空格字符
-    str = strings.Trim(str, " ")
-    if str == "" {
-        return 0, errors.New("Invalid input")
-    }
+	//过滤空格字符
+	str = strings.Trim(str, " ")
+	if str == "" {
+		return 0, errors.New("Invalid input")
+	}
 
-    //正负号
-    sign := 1
-    if str[0] == '+' {
-        str = str[1:]
-    } else if string(str[0]) == "-" {
-        str = str[1:]
-        sign = -1
-    }
+	//正负号
+	sign := 1
+	if str[0] == '+' {
+		str = str[1:]
+	} else if string(str[0]) == "-" {
+		str = str[1:]
+		sign = -1
+	}
 
-    //仅有正负号
-    if len(str) == 0 {
-        return 0, errors.New("Invalid input")
-    }
+	//仅有正负号
+	if len(str) == 0 {
+		return 0, errors.New("Invalid input")
+	}
 
-    ret := 0
-    addFlag := false //小数进位标记
-    for i:=0; i<len(str); i++ {
-        if str[i] >= '0' && str[i] <= '9' {
-            ret = ret * 10 + int(str[i] - '0')
-        } else if string(str[i]) == "." {
-            if i+1 <= len(str) - 1 && str[i+1] >= '0' && str[i+1] <= '9'{
-                if str[i+1] <= '4' {
-                    addFlag = false
-                } else {
-                    addFlag = true
-                }
-                break
-            }else {
-                return 0, errors.New("Invalid input")
-            }
-        } else {
-            return 0, errors.New("Invalid input")
-        }
-    }
+	ret := 0
+	addFlag := false //小数进位标记
+	for i := 0; i < len(str); i++ {
+		if str[i] >= '0' && str[i] <= '9' {
+			ret = ret*10 + int(str[i]-'0')
+		} else if string(str[i]) == "." {
+			if i+1 <= len(str)-1 && str[i+1] >= '0' && str[i+1] <= '9' {
+				if str[i+1] <= '4' {
+					addFlag = false
+				} else {
+					addFlag = true
+				}
+				break
+			} else {
+				return 0, errors.New("Invalid input")
+			}
+		} else {
+			return 0, errors.New("Invalid input")
+		}
+	}
 
-    if checkOverflow(ret, sign==-1) {
-        return 0, errors.New("Over flow")
-    }
+	if checkOverflow(ret, sign == -1) {
+		return 0, errors.New("Over flow")
+	}
 
-    if addFlag {
-        return sign * (ret + 1), nil
-    }
+	if addFlag {
+		return sign * (ret + 1), nil
+	}
 
-    return sign * ret, nil
+	return sign * ret, nil
 }
--- chapter7/common_parent.go
+++ chapter7/common_parent.go
@@ -5,36 +5,37 @@
 package chapter7
 
 import (
-    "github.com/hq-cml/sward2offer/basic"
-    "github.com/hq-cml/sward2offer/common"
+	"github.com/hq-cml/sward2offer/basic"
+	"github.com/hq-cml/sward2offer/common"
 )
 
 // 思路：
-// 1. 如果是二叉搜索树，这是一个递归的思路。如果两个节点都比根小，则递归左子树；如果都比根大，递归右子树；如果一大一小，答案就是根。（利用了二叉搜索树的性质）
+// 1. 如果是二叉搜索树，这是一个递归的思路。如果两个节点都比根小，则递归左子树；
+//    如果都比根大，递归右子树；如果一大一小，答案就是根。（利用了二叉搜索树的性质）
 // 2. 如果是包含指向父节点的指针，那么这个题就平移成了求两个链表的首个汇合节点。这个前面有
 // 3. 如果上述特殊条件都没有，那么就分别求出从根到两个节点各自的路径，然后从头开始找到第一个共同元素即可。
 func FindCommonParent(root *common.TreeNode, num1, num2 int) (int, bool) {
-    path1 := []int{}
-    path2 := []int{}
+	path1 := []int{}
+	path2 := []int{}
 
-    path1, ok1 := basic.FindPath(root, num1, path1)
-    path2, ok2 := basic.FindPath(root, num2, path2)
+	path1, ok1 := basic.FindPath(root, num1, path1)
+	path2, ok2 := basic.FindPath(root, num2, path2)
 
-    if !ok1 || !ok2 {
-        return -1, false
-    }
+	if !ok1 || !ok2 {
+		return -1, false
+	}
 
-    for k, v := range path1 {
-        if k > len(path2)-1 {
-            //paht1长
-            return path1[k-1], true;
-        }
-        //第一次出现不同，则上一个节点是公共节点
-        if v != path2[k] {
-            return path1[k-1], true;
-        }
-    }
+	for k, v := range path1 {
+		if k > len(path2)-1 {
+			//paht1长
+			return path1[k-1], true
+		}
+		//第一次出现不同，则上一个节点是公共节点
+		if v != path2[k] {
+			return path1[k-1], true
+		}
+	}
 
-    //到这里说明path1比path2短，则公共节点应该是path1的最后一个元素
-    return path1[len(path1)-1], true
+	//到这里说明path1比path2短，则公共节点应该是path1的最后一个元素
+	return path1[len(path1)-1], true
 }
