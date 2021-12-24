b6bc4923c32bceb54e0b0d558f269a12476c13c8
hq-cml <hq_cml@163.com>
2021-12-24 12:41:35 +0800

    comment

--- basic/common.go
+++ basic/common.go
@@ -1,8 +1,9 @@
 package basic
 
-//分区函数
-//返回：一次分区完毕之后的中间Idx值，保证Idx前面的均小于Idx元素值，后面的均大于
+// 分区函数
+// 返回：一次分区完毕之后的中间Idx值，保证Idx前面的均小于Idx元素值，后面的均大于
 func Partition(arr []int, begIdx, endIdx int) int {
+	// 边界校验
 	if len(arr) == 0 {
 		return -1
 	}
@@ -10,20 +11,26 @@ func Partition(arr []int, begIdx, endIdx int) int {
 		return -1
 	}
 
+	// 第一个值作为基准
 	val := arr[begIdx]
 
+	// 进行多轮次前后逼近，直到相遇
 	for endIdx > begIdx {
+		// 从后向前逼近
 		for arr[endIdx] >= val && endIdx > begIdx {
 			endIdx--
 		}
-		//swap
-		arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
+		if endIdx != begIdx { // 说明必然 arr[endIdx] < val
+			arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx] //swap
+		}
 
+		// 从前向后逼近
 		for arr[begIdx] <= val && endIdx > begIdx {
 			begIdx++
 		}
-		//swap
-		arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx]
+		if begIdx != endIdx { // 说明必然 arr[begIdx] > val
+			arr[begIdx], arr[endIdx] = arr[endIdx], arr[begIdx] //swap
+		}
 	}
 
 	return begIdx
