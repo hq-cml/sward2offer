42a2271226365c1acc9c44501a3f68deeca4d88a
hq <hq_cml@163.com>
2023-08-21 08:51:45 +0800

    leetcode hot100

---
+++ chapter5/_47_max_gift_value/min_gift.go
@@ -0,0 +1,38 @@
+/**
+ * 最小值，方正证券题目
+ */
+package _47_max_gift_value
+
+import "math"
+
+func MinGift(values [][]int) int {
+	rows := len(values)
+	if rows == 0 {
+		return 0
+	}
+	cols := len(values[0])
+	if cols == 0 {
+		return 0
+	}
+
+	return minGift(values, rows-1, cols-1)
+}
+
+func minGift(values [][]int, i, j int) int {
+	if i < 0 || j < 0 {
+		// 这里巨坑，很容易就思考错误了。不是返回0，而应该是返回以一个最大值！！因为整个题目求得是最小值，0可能会被错误的参与计算
+		return math.MaxInt
+	}
+	if i == 0 && j == 0 {
+		return values[0][0]
+	}
+	min := Min(minGift(values, i-1, j), minGift(values, i, j-1))
+	return values[i][j] + min
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ chapter5/_47_max_gift_value/min_gift_test.go
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+package _47_max_gift_value
+
+import "testing"
+
+func TestMinGift(t *testing.T) {
+	type args struct {
+		values [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case1",
+			args: args{
+				values: [][]int{
+					{1, 3, 1},
+					{1, 5, 1},
+					{4, 2, 1},
+				},
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MinGift(tt.args.values); got != tt.want {
+				t.Errorf("MinGift() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
