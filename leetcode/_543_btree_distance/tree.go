ffd5f057e6ef97aa78fd02e5be5f171d5fbb9e22
hq <hq_cml@163.com>
2023-09-30 15:56:21 +0800

    leetcode hot100

--- leetcode/_543_btree_distance/tree.go
+++ leetcode/_543_btree_distance/tree.go
@@ -1,19 +1,14 @@
 /*
-*
-
-	*给你一棵二叉树的根节点，返回该树的 直径 。
-
-二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
-
-两节点之间路径的 长度 由它们之间边数表示。
-*/
+ * 给你一棵二叉树的根节点，返回该树的 直径 。
+ * 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+ * 两节点之间路径的 长度 由它们之间边数表示。
+ */
 package _543_btree_distance
 
 import "github.com/hq-cml/sward2offer/common"
 
 // 思路：本质上是树的遍历 + 距离求值
-//
-//	距离求值：某个节点为根的距离，实际上就是左子树高 + 右子树高
+// 距离求值：某个节点为根的距离，实际上就是左子树高 + 右子树高
 func diameterOfBinaryTree(root *common.TreeNode) int {
 	max := 0
 	var dfs func(*common.TreeNode)
